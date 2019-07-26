a74ab01632789c0d69701bfb6d49e13886c63f38
Lynn Smeria <ae@cephea.de>
2019-07-26 18:38:56 +0800

    Add Ghibli example with custom nodes

---
+++ examples/custom-nodes/ExampleNode.js
@@ -0,0 +1,88 @@
+/**
+ * A Cocoon node is essentially a pure function wrapped in an object that
+ * contains some meta information, exported according to ES2015 standards.
+ *
+ * No library or API imports required! It's that simple.
+ */
+
+/**
+ * Nodes can have their own dependencies, which means you have the whole npm
+ * ecosystem available to your fingertips. Just add them to your package.json
+ * and require them like usual.
+ *
+ * A handful of dependencies are already included by Cocoon and don't need to be
+ * required or installed, lodash being one of them. We could leave the next line
+ * out, it's only there so the TS type-checker can load the correct typings.
+ */
+const _ = require('lodash');
+
+/**
+ * Note that it's necessary that nodes are named exports. The name of the export
+ * determines the name of the node. The filename is irrelevant, and it's
+ * possible to export multiple nodes per file.
+ */
+module.exports.ExampleNode = {
+  /**
+   * The category is used to categorise nodes into groups (as seen in the
+   * editor's context menu). This is optional.
+   */
+  category: 'Data',
+
+  /**
+   * Describes what the node does. Optional but highly recommended.
+   */
+  description: `A custom Cocoon node that picks a random item from the data, for demonstration purposes.`,
+
+  /**
+   * The input ports. This tells the editor what node are available and can be
+   * used to attach various meta information to the port.
+   */
+  in: {
+    data: {
+      description: `An array of data item objects.`,
+    },
+  },
+
+  /**
+   * Same as `in`, but for output ports.
+   */
+  out: {
+    item: {
+      description: `The randomly picked data item.`,
+    },
+  },
+
+  /**
+   * This is the most important part of the node. Like the name of the function
+   * implies, this is where the actual data processing happens.
+   *
+   * The node receives a context objects that contains the entire graph, the
+   * node registry, port interface and various other things. It encapsulates the
+   * entire state of Cocoon.
+   *
+   * It should always be an async generator function, even when await/yield is
+   * not used, but regular functions will work as well. We will explore these
+   * concepts later in this example.
+   *
+   * If you want to have a look at the typings, see `CocoonNodeContext` at:
+   * https://unpkg.com/@cocoon/types/dist/index.d.ts
+   */
+  async *process(context) {
+    // Read the data from the input port. This just fetches a reference, so the
+    // operation is cheap and fast.
+    const { data } = context.ports.read();
+
+    // Sample a random item using lodash
+    const randomItem = _.sample(data);
+
+    // Write the data to the output ports. Caches are created after the
+    // processing finishes, so this is another cheap call at this point.
+    context.ports.write({ item: randomItem });
+
+    // Processing functions don't have to return anything, but can yield/return
+    // either a string or an array of the form [string, number], the number
+    // being the progress in percent (0 to 1). They're used to show visual
+    // status and progress indicators in the editor.
+    return `You should watch ${randomItem.title}!`;
+  },
+};
---
+++ examples/custom-nodes/Wikipedia.js
@@ -0,0 +1,126 @@
+/**
+ * Another custom node. Make sure to have a look at the documentation in
+ * `ExampleNode.js` first, so that you're familiar with the basics.
+ */
+
+const _ = require('lodash');
+const got = require('got');
+
+module.exports.Wikipedia = {
+  category: 'I/O',
+  description: `A custom Cocoon node that retrieves information via the Wikipedia API.`,
+
+  in: {
+    data: {
+      description: `An array of Ghibli movie data.`,
+    },
+  },
+
+  out: {
+    data: {
+      description: `The input data, annotated with Wikipedia information.`,
+    },
+  },
+
+  async *process(context) {
+    const { data } = context.ports.read();
+
+    /**
+     * For performance reasons, the data in Cocoon is passed via reference
+     * instead of making deep copies between every node. But we want to avoid
+     * changing data in a way that would change it in the cache of previous
+     * nodes as well, which could cause subtle issues in your processing
+     * pipeline.
+     *
+     * There's various ways to create deep copies in Cocoon, the easiest of
+     * which is annotating the input port with `clone: true`.
+     *
+     * But in this case it's sufficient to just shallow-copy the data since we
+     * will write new objects to it.
+     */
+    const shallowDataCopy = [...data];
+
+    for (let i = 0; i < data.length; i++) {
+      /**
+       * Remember that Cocoon processors are generator functions. That means we
+       * can yield a progress report at any time, so that we can see it in the
+       * editor.
+       *
+       * But it has another benefit: since Cocoon processing runs on a single
+       * thread, to avoid expensive data serialisation, long processing tasks
+       * can make the Cocoon process unresponsive. Breaking processing up into
+       * smaller chunks also makes the processing interruptible.
+       *
+       * So yield often! The performance overhead is small, since UI updates are
+       * throttled.
+       */
+      yield `Querying info for ${data[i].title}`;
+
+      // Example query:
+      // https://en.wikipedia.org/w/api.php?action=query&titles=My+Neighbor+Totoro&prop=images
+      const pageInfo = await queryWikipedia(
+        context,
+        `titles=${encodeURIComponent(data[i].title)}&prop=images`
+      );
+
+      /**
+       * It's not trivial to select the poster image. Wikipedia lists the images
+       * in order, but the infobox image could be at any position. Our best
+       * approach is to pick an image that contains the title.
+       *
+       * Fortunately for us, Cocoon has a node that can calculate a variety of
+       * distance metrics, and even combine them to form complex
+       * multi-dimensional distances.
+       *
+       * Cocoon nodes can use temporary nodes as part of its processing (since
+       * nodes are really just functions). That way you can avoid building and
+       * repeating and overly complex graphs in the Cocoon editor itself.
+       */
+      const result = await context.processTemporaryNode('Distance', {
+        affluent: pageInfo.images.map(x => ({ title: x.title })),
+        attribute: 'related',
+        data: [{ title: data[i].title }],
+        metrics: {
+          title: { type: 'String' },
+        },
+      });
+      const bestImage = result.data[0].related[0].title;
+      context.debug(
+        `calculated title distances, best match is: ${bestImage}`,
+        result
+      );
+
+      // Example query:
+      // https://en.wikipedia.org/w/api.php?action=query&titles=File:My%20Neighbor%20Totoro%20-%20Tonari%20no%20Totoro%20(Movie%20Poster).jpg&prop=imageinfo&iiprop=url
+      const imageInfo = await queryWikipedia(
+        context,
+        `titles=${encodeURIComponent(bestImage)}&prop=imageinfo&iiprop=url`
+      );
+
+      /**
+       * It's usually good practice to construct a new object using the
+       * spread-syntax, unless you're dealing with memory constraints or need to
+       * optimise for performance. It's the least error prone way of annotating
+       * the original data objects. Don't optimise prematurely!
+       */
+      shallowDataCopy[i] = {
+        ...data[i],
+        wikipedia: {
+          ...pageInfo,
+          ...imageInfo,
+        },
+      };
+    }
+
+    context.ports.write({ data: shallowDataCopy });
+  },
+};
+
+async function queryWikipedia(context, query) {
+  const url = `https://en.wikipedia.org/w/api.php?action=query&${query}&format=json`;
+  context.debug(`querying ${url}`);
+  const results = await got(url, { json: true });
+  context.debug(`got results:`, results.body);
+  const pages = results.body.query.pages;
+  return pages ? pages[Object.keys(pages)[0]] : null;
+}
